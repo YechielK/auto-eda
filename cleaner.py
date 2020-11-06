4663a25d18beddbbf066250ebd865463ddca64ea
YechielK <thespecialk1@gmail.com>
2020-11-05 22:00:06 -0500

    removed prints

--- cleaner.py
+++ cleaner.py
@@ -5,7 +5,6 @@ import scipy.stats as stats
 from flask import g
 def clean(df):
     g.before_shape = df.shape
-    print(df.dtypes)
     # rename cols
     df = col_fix(df)
     # remove duplicates
@@ -27,19 +26,16 @@ def name_fix(df):
     cols = list(df.columns)
     newCols = {}
     for col in cols:
-        print(col)
         newCols[col] = col.lower().replace(' ', '_')
     return newCols
 
 def remove_nulls(df):
     nulls = df.isnull().sum().sum()
-    print('contains ', nulls, ' nulls')
     g.nulls = nulls
     return df.dropna()
 
 def remove_duplicates(df):
     duplicates = df.duplicated().sum()
-    print('contains ', duplicates, ' duplicates')
     g.duplicates = duplicates
     return df.drop_duplicates()
 
@@ -58,7 +54,6 @@ def remove_outliers(df):
         df[col_zscore] = abs(stats.stats.zscore(df[col]))
         z_score_cols.append(col_zscore)
 
-    print('Before removal of outliers', df.shape)
     before_rows = df.shape[0]
     # row is false when it contains zscore > 3
     condition = (df[z_score_cols] < 3).all(axis=1)
@@ -66,8 +61,6 @@ def remove_outliers(df):
 
     # remove zscore cols
     df = df[original_cols]
-    print('After removal of outliers', df.shape)
     outliers = before_rows - df.shape[0]
-    print('contains ', outliers, ' outliers')
     g.outliers = outliers
     return df
