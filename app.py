699173de455c86418a719c5e1aa90aeeca24154a
YechielK <thespecialk1@gmail.com>
2020-11-10 13:55:05 -0500

    updated data routing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, logging, redirect, url_for, Response, g
+from flask import Flask, render_template, request, logging, redirect, url_for, Response, g, session
 from werkzeug.utils import secure_filename
 import pandas as pd
 import numpy as np
@@ -6,36 +6,35 @@ import csv
 import os
 import cleaner
 import linreg
-
+import pickle
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "open secret"
 
 
-x = 'this is x'
-y = 'this is y'
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 @app.route('/data', methods=['GET', 'POST'])
 def data():
-    global x
-    global y
+
     if request.method == 'POST':
 
         file = request.files['file']
-        y = request.form['t']
-        y = cleaner.fix_target(y)
+        session['filename'] = file.filename
+        print('filename', session['filename'] )
+        session['target'] = request.form['target']
+        session['target'] = cleaner.fix_target(session['target'])
         data = pd.read_csv(file)
         data = cleaner.clean(data)
-        x = data
-        
-        # return render_template('data.html', data=data)
+
+        data.to_pickle(session['filename'])
+
         return render_template(
             'data.html',
             tables=[data.head().to_html()],
-            # titles=data.columns.values,
             nulls=g.nulls,
             duplicates=g.duplicates,
             outliers=g.outliers,
@@ -43,33 +42,36 @@ def data():
 
 @app.route('/getPlotCSV', methods=['GET', 'POST'])
 def getPlotCSV():
-    global x
 
+    data = pd.read_pickle(session['filename'])
     return Response(
-        x.to_csv(index=False),
+        data.to_csv(index=False),
         mimetype="text/csv",
         headers={"Content-disposition":
                  "attachment; filename=myplot.csv"})
 
 @app.route('/linearRegression', methods=['GET','POST'])
 def linearRegression():
-    global x
-    global y
+    
+    data = pd.read_pickle(session['filename'])
 
+    os.remove(session['filename'])
 
-
-    linreg_model = linreg.linreg(x,y)
+    linreg_model = linreg.linreg(data,session['target'])
     print(linreg_model.coef_)
     print(linreg_model.intercept_)
+        
 
 
     ans = ''
+    eq = g.selected_features + ' * ' + str(linreg_model.coef_[0])
+
     if np.sign(linreg_model.coef_) > 0:
-        ans = '+ ' + g.selected_features + ' * ' + str(linreg_model.coef_[0])
+        ans = '+ ' + eq
     else:
-        ans = '- ' + g.selected_features + ' * ' + str(linreg_model.coef_[0])
+        ans = '- ' + eq
 
-
+    
 
     return render_template(
             'linearreg.html',
