a9d00867bb7a1b2740688b7f81c7d2fdaa5c0bf3
YechielK <thespecialk1@gmail.com>
2020-12-31 06:23:03 -0500

    odds and ends

--- static/bayes.js
+++ static/bayes.js
@@ -1,35 +1,23 @@
-// function funcy() {
-//     console.log('hey fom js file')
-// }
-// function juicy() {
-//     console.log('poo')
-// }
-function frep(v,a,b) {
-    console.log('wer')
-    console.log(v)
-    console.log(a)
-    console.log(b)
-}
-// window.onload = funcy()
-
-// function fun() {
-//     console.log("we have fun");
-//   }
+function download() {
+    var download = document.getElementById("download");
+    var image = document.getElementById("myCanvas").toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+    download.setAttribute("href", image);
+    //download.setAttribute("download","archive.png");
+    }
 
 
-// function hope() {
-//     console.log('works')
-// }
-// console.log("in static 3")
+
 function draw_line(x, y, length, name, val, ctx, a, b) {
 
     width = length*1.44
+
     if (name == 'b_and_a') {
         x_start = x
         y_start = y
         x_end = x - (length/10)
         y_end = y- (length/10)
-        x_text = x - (length/5)
+        x_txt = x - (length/5)
         prob = "P(" + b + "^" + a + ")";
     }
 
@@ -38,7 +26,7 @@ function draw_line(x, y, length, name, val, ctx, a, b) {
         y_start = y + length
         x_end = x - (length/10)
         y_end = y + length + (length/10)
-        x_text = x - (length/5)
+        x_txt = x - (length/5)
         prob = "P(" + b + "`^" + a + ")";
     }
     
@@ -47,7 +35,7 @@ function draw_line(x, y, length, name, val, ctx, a, b) {
         y_start = y
         x_end = x + length + (length/10)
         y_end = y- (length/10)
-        x_text = x + length + (length/10)
+        x_txt = x + length + (length/10)
         prob = "P(" + b + "^" + a + "`)";
     }
 
@@ -56,7 +44,7 @@ function draw_line(x, y, length, name, val, ctx, a, b) {
         y_start = y + length
         x_end = x + length + (length/10)
         y_end = y + length + (length/10)
-        x_text = x + length + (length/10)
+        x_txt = x + length + (length/10)
         prob = "P(" + b + "`^" + a + "`)";
     }
 
@@ -69,15 +57,16 @@ function draw_line(x, y, length, name, val, ctx, a, b) {
     ctx.font = width/48  + "px Arial";
 
     // draw prob name
-    ctx.fillText(prob, x_text, y_end);
+    ctx.fillText(prob, x_txt, y_end);
 
     //draw prob val
-    ctx.fillText(val, x_text , y_end + width/36);
-
+    ctx.fillText(val, x_txt , y_end + width/36);
 }
 
 function draw_triangle(x, y, length, name, val, ctx, a, b) {
+    
     width = length*1.44
+    
     if (name == 'b_given_not_a') {
         x_hi_start = x + length
         y_hi_start = y 
@@ -85,7 +74,7 @@ function draw_triangle(x, y, length, name, val, ctx, a, b) {
         y_lo_start = y + (length * val)
         x_end = x + length + (length/10)
         y_end = y + (length * val)/2
-        x_text =  x + length + (length/10)
+        x_txt =  x + length + (length/10)
         prob = "P(" + b + "|" + a + "`)";
     }
 
@@ -96,7 +85,7 @@ function draw_triangle(x, y, length, name, val, ctx, a, b) {
         y_lo_start = y +  length
         x_end = x + length + (length/10)
         y_end = y + (length * ((1-val) + val/2))
-        x_text =  x + length + (length/10)
+        x_txt =  x + length + (length/10)
         prob = "P(" + b + "`|" + a + "`)";
     }
 
@@ -107,7 +96,7 @@ function draw_triangle(x, y, length, name, val, ctx, a, b) {
         y_lo_start = y + (length *val)
         x_end = x - (length/10)
         y_end = y+ (length * val)/2;
-        x_text =  x - (length/5)
+        x_txt =  x - (length/5)
         prob = "P(" + b + "|" + a + ")";
     }
 
@@ -118,7 +107,7 @@ function draw_triangle(x, y, length, name, val, ctx, a, b) {
         y_lo_start = y + length
         x_end = x - (length)/10
         y_end = y + (length * ((1-val) + val/2))
-        x_text =  x - (length/5)
+        x_txt =  x - (length/5)
         prob = "P(" + b + "`|" + a + ")";
     }
 
@@ -129,7 +118,7 @@ function draw_triangle(x, y, length, name, val, ctx, a, b) {
         y_lo_start = y 
         x_end = x + (length *val)/2
         y_end = y- (length/10)
-        x_text =  x + (length * val)/2 
+        x_txt =  x + (length * val)/2 
         prob = "P(" + a + ")" ;
     }
 
@@ -140,10 +129,9 @@ function draw_triangle(x, y, length, name, val, ctx, a, b) {
         y_lo_start = y 
         x_end = x + (length * (1-val))/2 + (length)/2
         y_end = y - (length/10)
-        x_text =  x + (length * (1-val))/2 + (length)/2
+        x_txt =  x + (length * (1-val))/2 + (length)/2
         prob = "P(" + a + "`)" ;
     }
-
 
     // top line
     ctx.beginPath();
@@ -160,10 +148,10 @@ function draw_triangle(x, y, length, name, val, ctx, a, b) {
     ctx.font = width/48  + "px Arial";
 
     // probability
-    ctx.fillText(prob, x_text, y_end);
+    ctx.fillText(prob, x_txt, y_end);
     
     // val
-    ctx.fillText(val, x_text, y_end + width/36);
+    ctx.fillText(val, x_txt, y_end + width/36);
 }
 
 function draw_square(v, a, b) {
@@ -176,7 +164,7 @@ function draw_square(v, a, b) {
     x = length/5
     y = length/5
     origin = 0
-    circle_size = .8
+    circle_size = .2
 
     ctx.beginPath();
     ctx.rect(x, y, length, length);
@@ -188,7 +176,7 @@ function draw_square(v, a, b) {
     // P(A) and P(A`)
     ctx.beginPath();
     ctx.moveTo(x + (length * v['a']), y);
-    ctx.lineTo(x + (length * v['a']),y + length);
+    ctx.lineTo(x + (length * v['a']), y + length);
     ctx.stroke();
 
     // P(B|A) and P(B`|A)
@@ -199,7 +187,7 @@ function draw_square(v, a, b) {
 
     // P(B|A`) and P(B`|A`)
     ctx.beginPath();
-    ctx.moveTo(x + (length * v['a'] ),y + (length * v['b_given_not_a'] ));
+    ctx.moveTo(x + (length * v['a'] ), y + (length * v['b_given_not_a'] ));
     ctx.lineTo(x + length, y +  (length * v['b_given_not_a'] ));
     ctx.stroke();
 
@@ -211,5 +199,51 @@ function draw_square(v, a, b) {
         draw_line(x, y, length, ands[i], v[ands[i]], ctx, a, b)
     }
 
-
+    // TODO
+    // Fix ration of amount of circles so lines that cut through are
+    //circles
+    for (let i = x + length/(2 * m); i <x + length; i+=length/(1 * m)) {
+        for (let j = y + length/(2 * m); j <y + length; j+=length/(1 * m)) {
+            ctx.beginPath();
+            ctx.arc(j, i, length/(2 * m) * circle_size, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
     }
+}
+
+// function draw_circle() {
+//     var canvas = document.getElementById("myCanvas");
+//     var ctx = canvas.getContext("2d");
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     // Colors
+//     var colors = ['#4CAF50', '#00BCD4', '#E91E63', '#FFC107', '#9E9E9E', '#CDDC39'];
+//     var angles = [Math.PI * 2 * {{ v['b_and_a']}}, Math.PI * 2 * {{ v['not_b_and_a']}}, Math.PI * 2 * {{ v['b_and_not_a']}}, Math.PI * 2 * {{ v['not_b_and_not_a']}}];
+//     d = canvas.width
+//     // Temporary variables, to store each arc angles
+//     var beginAngle = 0;
+//     var endAngle = 0;
+  
+//     // Iterate through the angles
+//     for(var i = 0; i < angles.length; i++) {
+//         // Begin where we left off
+//         beginAngle = endAngle;
+//         // End Angle
+//         endAngle = endAngle + angles[i];
+        
+//         ctx.beginPath();
+//         // Fill color
+//         ctx.fillStyle = colors[i % colors.length];
+        
+//         // Same code as before
+//         ctx.moveTo(d * .4, d * .4);
+//         ctx.arc(d * .4, d * .4, d * .24, beginAngle, endAngle);
+//         ctx.lineTo(d * .4, d * .4);
+//         ctx.stroke();
+        
+//         // Fill
+//         ctx.fill();
+//     }
+// }
+
+
+
